ec12511737d21dc64a181bc1b990332e4483db50
flyhawksz <flyhawksz@163.com>
2019-10-20 20:44:54 +0800

    edit Class graph, create BFS, with data

--- get_exchanges_ticker/BSF.py
+++ get_exchanges_ticker/BSF.py
@@ -10,48 +10,83 @@ from queue import Queue
 import numpy as np
 
 
-def BFS(graph:GraphMatrix, start):
+def BFS(graph:GraphMatrix, start, value):
+    """
+    广度遍历
+    每次访问邻居，更新value, 如果更大，则邻居的value 放新的值
+    :param graph: 图
+    :param start: 开始的序号
+    :param value: 开始的金额
+    :return:
+    """
+    #初始化
     # queue本质上是堆栈，用来存放需要进行遍历的数据
     # order里面存放的是具体的访问路径
+    inf = float('inf')
     queue = Queue()
     visited = [False]*graph.num_vertices
-    order = []
+    pre_node = [None]*graph.num_vertices
+    currency_value = dict()
+    currency_value[start] = value
+
+    for v in range(graph.num_vertices):
+        currency_value[v] = -inf
+
     # 首先将初始遍历的节点放到queue中，表示将要从这个点开始遍历
     # 由于是广度优先，也就是先访问初始节点的所有的子节点，所以可以
+    currency_value[start] = value
     queue.put(start)
-    order.append(start)
     visited[start] = True
-    while queue:
-        # queue.pop(0)意味着是队列的方式出元素，就是先进先出，而下面的for循环将节点v的所有子节点
+
+    queue.put(start)
+    while not queue.empty():
+        # 队列的方式出元素，就是先进先出，而下面的for循环将节点v的所有子节点
         # 放到queue中，所以queue.pop(0)就实现了每次访问都是先将元素的子节点访问完毕，而不是优先叶子节点
-        v = queue.pop(0)
-        for w in self.sequense[v]:
-            if w not in order:
-                # 这里可以直接order.append(w) 因为广度优先就是先访问节点的所有下级子节点，所以可以
-                # 将self.sequense[v]的值直接全部先给到order
-                order.append(w)
-                queue.append(w)
-    return order
+        current_node = queue.get()
+        current_value = currency_value[current_node]
+        print(graph.vertices[current_node])
+        for node in graph.vertex_neighbors_index[current_node]:
+            node_value = currency_value[node]
+            # 如果可以兑换
+            if graph.matrix[current_node][node] > 0:
+                # 兑换后的值
+                new_value = current_value * graph.matrix[current_node][node]
+                # 如果兑换后的值比原有的值大，更新值，并设下一个节点为未访问，可以重新按新值测算
+                if new_value > node_value:
+                    currency_value[node] = new_value
+                    # 避免出现负环情况（在此处是正环）
+                    # if node != start:
+                    #     visited[node] = False
+                    # 更新前序为当前节点
+                    pre_node[node] = current_node
+                    if node == start:
+                        return currency_value, pre_node
+                # 将需访问节点加入队列
+                if not visited[node]:
+                    print('下一个后续是： {}'.format(graph.vertices[node]))
+                    visited[node] = True
+                    queue.put(node)
+                    print('前序是： {}'.format(graph.vertices[current_node]))
+    print('遍历完毕')
+    return currency_value, pre_node
 
-
-# ————————————————
-# 版权声明：本文为CSDN博主「changyuanchn」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
-# 原文链接：https://blog.csdn.net/changyuanchn/article/details/79008760
 
 def load_from_txt():
     vertices_txt = 'vertices.txt'
     matrix_txt = 'matrix.txt'
     nodes = np.loadtxt(vertices_txt, dtype=bytes).astype(str)
-    print(nodes)
+    # print(nodes)
     matrix = np.loadtxt(matrix_txt, dtype=bytes, delimiter=',').astype(float)
-    print(matrix)
-    my_graph = GraphMatrix(nodes, matrix)
-    print(my_graph)
+    # print(matrix)
+    my_graph = GraphMatrix(list(nodes), list(matrix))
+    # print(my_graph)
     return my_graph
-
-def main():
-    pass
 
 
 if __name__ == '__main__':
-    pass
+    g = load_from_txt()
+    currency_value, pre_node = BFS(g, 0, 1000)
+    print('*'*30)
+    print(currency_value)
+    print('-'*30)
+    print(pre_node)
